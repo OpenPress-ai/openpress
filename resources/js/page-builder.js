06593f2fa40eb4033c5447b0b93f4d9f2546de15
Christopher David <chris@openagents.com>
2024-10-14 13:57:26 -0500

    restructure dir

--- resources/js/page-builder.js
+++ resources/js/page-builder.js
@@ -1,2 +1,4 @@
 // This is a placeholder for the Page Builder JavaScript
-console.log('Page Builder loaded');
+import "../../dementor/dev/js/editor/editor";
+
+console.log('Page Builder loaded', window.elementor);
