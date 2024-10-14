ce20a9e79a912d8e0d100dbf45a03e2e12b262af
Christopher David <chris@openagents.com>
2024-10-13 21:42:13 -0500

    Add `wordpress_id` field to `posts` table to link with WordPress content.

--- database/migrations/2024_10_14_022521_create_posts_table.php
+++ database/migrations/2024_10_14_022521_create_posts_table.php
@@ -10,6 +10,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('wordpress_id')->unique();
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('mobiledoc');
