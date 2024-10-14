888906a74f8707956c60c570f969e596ab4c70d4
Christopher David <chris@openagents.com>
2024-10-14 10:09:10 -0500

    Add 'page-builder.css' and 'page-builder.js' to Vite config input list for better asset management.

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/page-builder.css',
+                'resources/js/page-builder.js',
             ],
             refresh: true,
         }),
