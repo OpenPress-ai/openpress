9e735bc2a4e29442f431865fefdd9ee14cb18bca
Christopher David <chris@openagents.com>
2024-10-13 21:28:48 -0500

    Add fillable fields and posts relationship to Tag model for enhanced functionality.

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
