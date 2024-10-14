93a3042d7e5bc525e0c335814a4e3a07031a8b3b
Christopher David <chris@openagents.com>
2024-10-13 21:54:35 -0500

    Add Hash import and static password to UserFactory for secure password handling.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,12 +4,18 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
     /**
      * Define the model's default state.
      *
@@ -21,9 +27,9 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'slug' => fake()->unique()->slug(), // Add this line
+            'slug' => fake()->unique()->slug(),
             'bio' => fake()->paragraph(),
             'website' => fake()->url(),
             'profile_image' => fake()->imageUrl(),
