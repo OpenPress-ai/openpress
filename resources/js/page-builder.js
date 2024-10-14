421ad7fe16db374a2e4df255707fd8f0dee39b8b
Christopher David <chris@openagents.com>
2024-10-14 13:58:05 -0500

    update that path

--- resources/js/page-builder.js
+++ resources/js/page-builder.js
@@ -1,4 +1,4 @@
 // This is a placeholder for the Page Builder JavaScript
-import "../../dementor/dev/js/editor/editor";
+import "../../dementor/assets/dev/js/editor/editor";
 
 console.log('Page Builder loaded', window.elementor);
