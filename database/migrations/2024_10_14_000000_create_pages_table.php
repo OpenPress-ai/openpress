e9e1fd77db323d5a90ee86975a26c96ff01d82bc
Christopher David <chris@openagents.com>
2024-10-14 10:08:12 -0500

    Create file database/migrations/2024_10_14_000000_create_pages_table.php

---
+++ database/migrations/2024_10_14_000000_create_pages_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->json('content');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+};
