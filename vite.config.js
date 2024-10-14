39fd71fedf11b6f5de96db3b395dafffe66026ed
Christopher David <chris@openagents.com>
2024-10-14 14:09:49 -0500

    Added `fs` module and `esbuild` config for JSX in `vite.config.js` to enhance JavaScript processing.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
 
 export default defineConfig({
     plugins: [
@@ -13,4 +14,28 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    esbuild: {
+        loader: 'jsx',
+        include: /resources\/.*\.js$/,
+        exclude: [],
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [
+                {
+                    name: 'load-js-files-as-jsx',
+                    setup(build) {
+                        build.onLoad({ filter: /resources\/.*\.js$/ }, async (args) => ({
+                            loader: 'jsx',
+                            contents: await fs.promises.readFile(args.path, 'utf8'),
+                        }));
+                        build.onLoad({ filter: /dementor\/.*\.js$/ }, async (args) => ({
+                            loader: 'jsx',
+                            contents: await fs.promises.readFile(args.path, 'utf8'),
+                        }));
+                    },
+                },
+            ],
+        },
+    },
 });
