1c8f70004fb33ef3a419cffa4847dd8036219c7a
Christopher David <chris@openagents.com>
2024-10-13 21:58:34 -0500

    Added 'email_verified_at' and 'password' fields to 'users' table to enhance user authentication.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->string('slug')->unique();
+            $table->string('slug')->nullable()->unique(); // Make slug nullable
             $table->text('bio')->nullable();
             $table->string('website')->nullable();
             $table->string('profile_image')->nullable();
