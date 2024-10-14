ccfa6c8c40c94681c4c46081a8e6f3e3672a0f9a
Christopher David <chris@openagents.com>
2024-10-13 20:58:02 -0500

    Simplify landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Welcome to OpenPress</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased bg-blue-100 flex items-center justify-center min-h-screen">
-        <div class="text-4xl text-gray-800 text-center">
-            Welcome to OpenPress
-        </div>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome to OpenPress</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="fixed w-full font-sans antialiased bg-blue-300 flex items-center justify-center min-h-screen">
+    <div class="text-4xl text-gray-800 text-center pointer-events-none select-none">
+        Welcome to OpenPress
+    </div>
+</body>
+
 </html>
