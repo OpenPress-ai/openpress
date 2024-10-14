24d6482e8a3f59da8589c1551de9b22dd4bb5df7
Christopher David <chris@openagents.com>
2024-10-14 10:03:40 -0500

    Refactor tests in AdminPanelTest.php to use Pest PHP syntax and add visibility assertions.

--- tests/Feature/PageBuilder/AdminPanelTest.php
+++ tests/Feature/PageBuilder/AdminPanelTest.php
@@ -1,81 +1,67 @@
 <?php
 
-namespace Tests\Feature\PageBuilder;
-
 use App\Models\User;
 use Spatie\Permission\Models\Role;
-use Tests\TestCase;
 
-class AdminPanelTest extends TestCase
-{
-    protected function setUp(): void
-    {
-        parent::setUp();
-        Role::create(['name' => 'admin']);
-    }
+beforeEach(function () {
+    Role::create(['name' => 'admin']);
+});
 
-    public function test_admin_can_see_page_builder_in_admin_panel()
-    {
-        $user = User::factory()->create();
-        $user->assignRole('admin');
+test('admin can see page builder in admin panel', function () {
+    $user = User::factory()->create();
+    $user->assignRole('admin');
 
-        $response = $this->actingAs($user)->get('/admin');
+    $response = $this->actingAs($user)->get('/admin');
 
-        $response->assertStatus(200);
-        $response->assertSee('Page Builder');
-    }
+    $response->assertStatus(200);
+    $response->assertSee('Page Builder');
+});
 
-    public function test_admin_can_access_page_builder_section()
-    {
-        $user = User::factory()->create();
-        $user->assignRole('admin');
+test('admin can access page builder section', function () {
+    $user = User::factory()->create();
+    $user->assignRole('admin');
 
-        $response = $this->actingAs($user)->get('/admin/page-builder');
+    $response = $this->actingAs($user)->get('/admin/page-builder');
 
-        $response->assertStatus(200);
-        $response->assertSee('Page Builder Dashboard');
-    }
+    $response->assertStatus(200);
+    $response->assertSee('Page Builder Dashboard');
+});
 
-    public function test_admin_can_see_list_of_pages_in_page_builder()
-    {
-        $user = User::factory()->create();
-        $user->assignRole('admin');
+test('admin can see list of pages in page builder', function () {
+    $user = User::factory()->create();
+    $user->assignRole('admin');
 
-        $response = $this->actingAs($user)->get('/admin/page-builder/pages');
+    $response = $this->actingAs($user)->get('/admin/page-builder/pages');
 
-        $response->assertStatus(200);
-        $response->assertSee('Pages');
-    }
+    $response->assertStatus(200);
+    $response->assertSee('Pages');
+});
 
-    public function test_admin_can_create_new_page_with_page_builder()
-    {
-        $user = User::factory()->create();
-        $user->assignRole('admin');
+test('admin can create new page with page builder', function () {
+    $user = User::factory()->create();
+    $user->assignRole('admin');
 
-        $response = $this->actingAs($user)->get('/admin/page-builder/pages/create');
+    $response = $this->actingAs($user)->get('/admin/page-builder/pages/create');
 
-        $response->assertStatus(200);
-        $response->assertSee('Create New Page');
-    }
+    $response->assertStatus(200);
+    $response->assertSee('Create New Page');
+});
 
-    public function test_admin_can_edit_existing_page_with_page_builder()
-    {
-        $user = User::factory()->create();
-        $user->assignRole('admin');
+test('admin can edit existing page with page builder', function () {
+    $user = User::factory()->create();
+    $user->assignRole('admin');
 
-        // Assume we have a page with ID 1
-        $response = $this->actingAs($user)->get('/admin/page-builder/pages/1/edit');
+    // Assume we have a page with ID 1
+    $response = $this->actingAs($user)->get('/admin/page-builder/pages/1/edit');
 
-        $response->assertStatus(200);
-        $response->assertSee('Edit Page');
-    }
+    $response->assertStatus(200);
+    $response->assertSee('Edit Page');
+});
 
-    public function test_non_admin_cannot_access_page_builder_section()
-    {
-        $user = User::factory()->create();
+test('non admin cannot access page builder section', function () {
+    $user = User::factory()->create();
 
-        $response = $this->actingAs($user)->get('/admin/page-builder');
+    $response = $this->actingAs($user)->get('/admin/page-builder');
 
-        $response->assertStatus(403);
-    }
-}
+    $response->assertStatus(403);
+});
