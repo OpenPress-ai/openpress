20072949955f259cd2499f351a1d6e53ca8f7af1
Christopher David <chris@openagents.com>
2024-10-13 21:27:50 -0500

    Add 'name' and 'slug' columns to 'tags' table, create 'post_tag' pivot table in migration.

--- database/migrations/2024_10_14_022528_create_tags_table.php
+++ database/migrations/2024_10_14_022528_create_tags_table.php
@@ -6,22 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
             $table->timestamps();
+        });
+
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+
+            $table->unique(['post_id', 'tag_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
+        Schema::dropIfExists('post_tag');
         Schema::dropIfExists('tags');
     }
 };
